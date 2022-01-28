a53e28f96636982ba1f8ae3950457a32aa5f1ca9
tanihassankuku <90456050+tanihassankuku@users.noreply.github.com>
2022-01-28 14:31:05 +0100

    Add files via upload

---
+++ mousePressed/sketch.js
@@ -0,0 +1,21 @@
+// flow / variables
+// Code! programming with p5.js
+
+
+let circleX = 100;
+
+function setup() {
+  createCanvas(400, 300);
+}
+function mousePressed(){
+ circleX =0; 
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  fill(255);
+  circle(circleX, 150,64);
+  
+  circleX = circleX + 5;
+}
