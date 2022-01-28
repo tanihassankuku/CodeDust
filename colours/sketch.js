cd852762b5c68b7421bca7d36552f7a12197de3e
tanihassankuku <90456050+tanihassankuku@users.noreply.github.com>
2022-01-28 14:24:46 +0100

    Add files via upload

---
+++ colours/sketch.js
@@ -0,0 +1,22 @@
+// flow / variables
+// Code! programming with p5.js
+
+let circleX = 100;
+
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  fill(255);
+  circle(circleX,150,64);
+}
+function person(){
+  var frist_name= "Tani"
+  var last_name= "kuku"
+  console.log(frist_name + last_name)
+  person() ; 
+  
+}
