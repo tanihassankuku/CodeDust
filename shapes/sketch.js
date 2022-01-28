a76c0eba48bd2095c133f363eb58ac69bb43a856
tanihassankuku <90456050+tanihassankuku@users.noreply.github.com>
2022-01-28 14:12:41 +0100

    Add files via upload

---
+++ shapes/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(220,0,200);
+  rectMode(CENTER);
+  circle(150,150,80);
+  triangle(30,75,58,20,86,75);
+  arc(50, 50, 80, 80, 0, PI + QUARTER_PI);
+
+  
+  
+}
