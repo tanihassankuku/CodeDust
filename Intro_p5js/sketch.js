b51787b3cb83e7215c2c50c8fea9d2e2a8aaf1f6
tanihassankuku <90456050+tanihassankuku@users.noreply.github.com>
2022-01-28 13:23:25 +0100

    Add files via upload

---
+++ Intro_p5js/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,200,00);
+  ellipse(50,46,80,80);
+  fill(100)
+  triangle(250,20,58,200,300,100);
+  fill(200,400,500); 
+}
