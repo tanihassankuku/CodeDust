4fad64dec0f28b031139475ab36cb401d9b5f822
tanihassankuku <90456050+tanihassankuku@users.noreply.github.com>
2022-02-04 15:15:46 +0300

    Add files via upload

---
+++ bouncing ball to all dricetoin/sketch.js
@@ -0,0 +1,26 @@
+let x = 320;
+let y = 180;
+let xspeed = 5;
+let yspeed = 2;
+
+
+let r = 25;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+
+   ellipse(x, y, r*2, r*2);
+  x += xspeed;
+  y += yspeed;
+  if (x > width - r || x < r) {
+    xspeed = -xspeed;
+  }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
+  }
+ 
+}
